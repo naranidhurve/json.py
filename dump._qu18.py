6df203b96b4a4d8d063bfc17a90c69da43329b02
Narani Dhurve <95540634+naranidhurve@users.noreply.github.com>
2022-09-08 02:32:23 -0400

    Add files via upload

---
+++ dump._qu18.py
@@ -0,0 +1,26 @@
+
+
+# import json
+# a={"name":"pavni" ,
+#    "class":"json" ,
+#    "age":4
+#    }
+# f=open('narani.json','w')
+# v=json.dump(a,f,indent=1)
+# print(v)
+
+
+# a="eshu"
+# b="nav"
+# c="gurukul"
+# d=2.5
+# e=int (d)
+# f=b+c
+# print(a,f)
+
+a="eshu"
+b="navgurukul"
+c="navgurukul"
+d=b+c
+e=a+d
+print(e)
